c116e4580648f494bc0a812f8e9adf55e3e001e7
Poonnachok chairuksa <ShienGameTV@gmail.com>
2020-09-06 15:08:26 +0700

    Add project files.

---
+++ HomeWorkProject11/Source.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int n, i, j, s;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n; j >= i; j--)
+        {
+            printf(" ");
+        }
+        s = i * 2 - 2;
+        while (s >= 0)
+        {
+            printf("*");
+            s--;
+        }
+
+        for (j = i; j <= n; j++)
+        {
+            printf(" ");
+        }
+
+        printf("\n");
+    }
+    for (i = n - 1; i > 0; i--)
+    {
+        for (j = n; j >= i; j--)
+        {
+            printf(" ");
+        }
+        s = i * 2 - 2;
+        while (s >= 0)
+        {
+            printf("*");
+            s--;
+        }
+        for (j = i; j <= n; j++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
